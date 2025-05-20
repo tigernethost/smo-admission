748334330a4c82af64042ab87f98247f298374e1
REYVIC-DEV <joshuavreyes.dev@gmail.com>
2025-05-20 13:18:32 +0800

    Student Information Tab Add Students

--- app/Http/Controllers/Admin/StudentCrudController.php
+++ app/Http/Controllers/Admin/StudentCrudController.php
@@ -34,12 +34,12 @@ class StudentCrudController extends CrudController
     /**
      * Define what happens when the List operation is loaded.
      *
-     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @see  https: //backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
     protected function setupListOperation()
     {
-        CRUD::setFromDb(); // set columns from db columns.
+        CRUD::setFromDb();  // set columns from db columns.
 
         /**
          * Columns can be defined using the fluent syntax:
@@ -50,14 +50,264 @@ class StudentCrudController extends CrudController
     /**
      * Define what happens when the Create operation is loaded.
      *
-     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @see https: //backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
     protected function setupCreateOperation()
     {
         CRUD::setValidation(StudentRequest::class);
         // CRUD::setFromDb(); // set fields from db columns.
+        CRUD::addField([
+            'name' => 'new_or_old',
+            'label' => 'Status',
+            'type' => 'select_from_array',
+            'options' => [
+                'New Student' => 'New Student',
+                'Old Student' => 'Old Student',
+            ],
+            'allows_null' => false,
+            'default' => 'New Student',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
 
+        CRUD::addField([
+            'name' => 'schoolyear',
+            'label' => 'School Year Entered',
+            'type' => 'select_from_array',
+            'options' => [
+                'YEAR' => 'YEAR',
+            ],
+            'allows_null' => false,
+            'attributes' => ['required' => 'required'],
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3 required'],
+        ]);
+
+        CRUD::addField([
+            'name' => 'application',
+            'label' => 'Application Date',
+            'type' => 'date_picker',
+            'tab' => 'Student Information',
+            'attributes' => ['required' => 'required'],
+            'wrapper' => ['class' => 'form-group col-md-3 required'],
+            'default' => now()->toDateString(),
+        ]);
+        /////
+        CRUD::addField([
+            'name' => 'div1',
+            'type' => 'custom_html',
+            'value' => '<div class="form-group col-xs-12 mb-0 p-0"></div>',
+            'tab' => 'Student Information',
+        ]);
+        /////
+        CRUD::addField([
+            'name' => 'department_id',
+            'label' => 'Department',
+            'type' => 'select_from_array',
+            'options' => [
+                1 => 'College of Arts',
+                2 => 'College of Science',
+                3 => 'College of Engineering',
+            ],
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+            'allows_null' => false,
+            'default' => 1,
+        ]);
+
+        CRUD::addField([
+            'name' => 'level_id',
+            'label' => 'Level',
+            'type' => 'select_from_array',
+            'options' => [
+                1 => '1',
+                2 => '2',
+                3 => '3',
+            ],
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+            'allows_null' => false,
+            'default' => 1,
+        ]);
+
+        CRUD::addField([
+            'name' => 'photo',
+            'label' => 'Photo',
+            'type' => 'image',
+            'tab' => 'Student Information',
+            'crop' => true,
+            'aspect_ratio' => 1,
+        ]);
+
+        CRUD::addField([
+            'name' => 'studentnumber',
+            'label' => 'Student Number',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['readonly' => 'true'],
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+
+        CRUD::addField([
+            'name' => 'lrn',
+            'label' => 'LRN',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+
+        /////
+        CRUD::addField([
+            'name' => 'div2',
+            'type' => 'custom_html',
+            'value' => '<div class="form-group col-xs-12 mb-0 p-0"></div>',
+            'tab' => 'Student Information',
+        ]);
+        /////
+
+        CRUD::addField([
+            'name' => 'last_name',
+            'label' => 'Last Name',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['required' => 'required'],
+            'wrapper' => ['class' => 'form-group col-md-3 required'],
+        ]);
+        CRUD::addField([
+            'name' => 'first_name',
+            'label' => 'First Name',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['required' => 'required'],
+            'wrapper' => ['class' => 'form-group col-md-3 required'],
+        ]);
+        CRUD::addField([
+            'name' => 'middle_name',
+            'label' => 'Middle Name',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+
+        CRUD::addField([
+            'name' => 'gender',
+            'label' => 'Gender',
+            'type' => 'select_from_array',
+            'options' => ['Male' => 'Male', 'Female' => 'Female'],
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+
+        /////
+        CRUD::addField([
+            'name' => 'div3',
+            'type' => 'custom_html',
+            'value' => '<div class="form-group col-xs-12 mb-0 p-0"></div>',
+            'tab' => 'Student Information',
+        ]);
+        /////
+
+        CRUD::addField([
+            'name' => 'citizenship',
+            'label' => 'Citizenship',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+        CRUD::addField([
+            'name' => 'religion',
+            'label' => 'Religion',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+        CRUD::addField([
+            'name' => 'contactnumber',
+            'label' => 'Contact Number',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+        CRUD::addField([
+            'name' => 'email',
+            'label' => 'Email',
+            'type' => 'email',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+        CRUD::addField([
+            'name' => 'birthplace',
+            'label' => 'Place of Birth',
+            'type' => 'email',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+        CRUD::addField([
+            'name' => 'birthdate',
+            'label' => 'Date of Birth',
+            'type' => 'email',
+            'tab' => 'Student Information',
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+        CRUD::addField([
+            'name' => 'age',
+            'label' => 'Age',
+            'type' => 'email',
+            'tab' => 'Student Information',
+            'attributes' => ['readonly' => 'true'],
+            'wrapper' => ['class' => 'form-group col-md-3'],
+        ]);
+
+        /////
+        CRUD::addField([
+            'name' => 'div4',
+            'type' => 'custom_html',
+            'value' => '<div class="form-group col-xs-12 mb-0 p-0">Residential Address In The Philippines</div>',
+            'tab' => 'Student Information',
+        ]);
+        /////
+
+        CRUD::addField([
+            'name' => 'province',
+            'label' => 'Province',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['required'=> true],
+            'wrapper' => ['class' => 'form-group col-md-2 required'],
+        ]);
+        CRUD::addField([
+            'name' => 'city_municipality',
+            'label' => 'City/Municipality',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['required'=> true],
+            'wrapper' => ['class' => 'form-group col-md-2 required'],
+        ]);
+        CRUD::addField([
+            'name' => 'barangay',
+            'label' => 'Barangay',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['required'=> true],
+            'wrapper' => ['class' => 'form-group col-md-2 required'],
+        ]);
+        CRUD::addField([
+            'name' => 'barangay',
+            'label' => 'Barangay',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['required'=> true],
+            'wrapper' => ['class' => 'form-group col-md-2 required'],
+        ]);
+        CRUD::addField([
+            'name' => 'residentialaddress',
+            // 'label' => 'Barangay',
+            'type' => 'text',
+            'tab' => 'Student Information',
+            'attributes' => ['required'=> true, 'placeholder'=>'Address',],
+            'wrapper' => ['class' => 'form-group col-md-6 required'],
+        ]);
         /**
          * Fields can be defined using the fluent syntax:
          * - CRUD::field('price')->type('number');
@@ -67,11 +317,19 @@ class StudentCrudController extends CrudController
     /**
      * Define what happens when the Update operation is loaded.
      *
-     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @see https: //backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
     protected function setupUpdateOperation()
     {
         $this->setupCreateOperation();
+    }
+
+    public function rules()
+    {
+        return [
+            'schoolyear' => 'required|string|max:255',
+            // other rules...
+        ];
     }
 }
